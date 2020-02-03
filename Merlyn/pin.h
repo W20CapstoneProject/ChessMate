2278b9188d0cf2641a9c66ad8adef649123da239
Erik Lewis <enlewis@ualberta.ca>
2020-02-02 17:25:38 -0700

    Initial commit of unfinished motor control code.

---
+++ Merlyn/pin.h
@@ -0,0 +1,19 @@
+#define stepPin_shoulder
+#define dirPin_shoulder
+#define enPin_shoulder
+
+#define stepPin_elbow
+#define dirPin_elbow
+#define enPin_elbow
+
+#define stepPin_wrist 
+#define dirPin_wrist
+#define enPin_wrist
+
+#define stepPin_base 
+#define dirPin_base
+#define enPin_base
+
+#define stepPin_roll 
+#define dirPin_roll
+#define enPin_roll
---
+++ Merlyn/stepper_config.h
@@ -0,0 +1,5 @@
+#define MAX_SPEED 100
+#define ACCELERATION 50
+#define INTERFACE_TYPE 1 //Driver interface
+#define NUM_STEPPERS 5
+#define STEP_ANGLE 1.8
