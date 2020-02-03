9c7550ca11caf8892b91309178eaf9483e4a2b1d
Steven Lagrange <slagrang@ualberta.ca>
2020-02-03 16:12:18 -0700

    Merged changes into master branch.

--- CMController/main.py
+++ CMController/main.py
@@ -1,6 +1,25 @@
 '''
 CMController
 
-Used to interpret ChessMate commands into meaningful commands for the Moveo Arm.
+Used to interpret ChessMate FEN based commands into coordinate commands for the Moveo Arm.
 
 '''
+
+
+
+'''
+getMove()
+
+Call to receive a coordinate command from the given FEN string indicating the desired chess move.
+'''
+def getCoordinateCommand(FEN):
+    return 0
+
+
+'''
+calculateDistance()
+
+Call to calculate the distance from the Moveo zero position to the chess square.
+'''
+def calculateDistance():
+    return 0
