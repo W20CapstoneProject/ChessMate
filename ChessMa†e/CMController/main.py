d72462d3eef5938dabcdad3685d84027360e0457
Steven Lagrange <slagrang@ualberta.ca>
2020-03-01 16:42:13 -0700

    Merging code bases.

---
+++ ChessMa†e/CMController/main.py
@@ -0,0 +1,9 @@
+'''
+Main
+
+Program that will be used in coordination with ChessMate.
+'''
+
+import cm
+import re
+import subprocess
---
+++ ChessMa†e/CMController/test.py
@@ -0,0 +1,30 @@
+'''
+Test
+
+Unit test suite for CMController actions.
+'''
+
+import unittest
+from cm import CMController
+from board import GameBoard
+
+
+class TestCMController(unittest.TestCase):
+
+    def setUp(self):
+        self.board = GameBoard()
+        self.cm = CMController()
+
+    def test_get_coordinate(self):
+        for index in range(1,65):
+            x = self.cm.get_coordinate_x(index)
+            y = self.cm.get_coordinate_y(index)
+            print("Square Number: " + str(index) + " ( "+str(x)+", " + str(y) + ")\n")
+
+        index = 4
+        cmd = self.cm.get_coordinate_command(index)
+        print(cmd)
+        self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+    unittest.main()
