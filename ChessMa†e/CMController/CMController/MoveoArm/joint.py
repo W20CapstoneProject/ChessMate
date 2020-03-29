72c472e6e00574291c3f1776296e7ce559d640b8
Steven Lagrange <slagrang@ualberta.ca>
2020-03-29 13:41:17 -0600

    Modified project structure. Implemented pytest within CMController. CMController is now an installible package. Added additional error checking in exectuing moves.

--- ChessMa†e/CMController/CMController/MoveoArm/joint.py
+++ ChessMa†e/CMController/CMController/MoveoArm/joint.py
@@ -1,6 +1,10 @@
 class Joint:
     '''
     Parent class for joint behaviour and constraint enforcement.
+    Each joint will be used to keep track of its own current position based on the command history while instantiated.
+    A typical command flow may be as follows:
+        1. Call calculate_steps() to determine the desired stepper motions.
+        2. Call issue_steps() to check the constraints of the joints, update current position
     '''
     def __init__(self, constraint, position, gear_ratio):
         self.constraint = constraint
@@ -10,6 +14,14 @@ class Joint:
         self.step_ratio = 0.9
         
 
+    def get_constraint(self):
+        return self.constraint
+
+
+    def set_position(self, position):
+        self.position = position
+
+
     def check_constraint(self, steps):
         '''
         Using the given constraint of the joint, check to see if move based on steps will fit within constraint.
@@ -18,6 +30,7 @@ class Joint:
             return True
         else:
             return False
+
 
     def issue_steps(self, steps):
         '''
@@ -38,36 +51,35 @@ class Joint:
         return (degrees / self.step_ratio) * self.gear_ratio
 
 
-
-class BaseJoint(Joint):
+class Base(Joint):
 # Base joint of Moveo arm.
-    def __init__(self, constraint = 30, position = 0, gear_ratio = 1.0):
+    def __init__(self, constraint = 200, position = 0, gear_ratio = 1.0):
         super().__init__(constraint, position, gear_ratio)
         
 
 
-class ShoulderJoint(Joint):
+class Shoulder(Joint):
 # Shoulder joint of Moveo arm.
-    def __init__(self, constraint = 30, position = 0, gear_ratio = 5.4):
+    def __init__(self, constraint = 200, position = 0, gear_ratio = 5.4):
         super().__init__(constraint, position, gear_ratio)
    
 
-class ElbowJoint(Joint):
+class Elbow(Joint):
 # Elbow joint of Moveo arm.
-    def __init__(self, constraint = 30, position = 0, gear_ratio = 1.0):
+    def __init__(self, constraint = 200, position = 0, gear_ratio = 1.0):
         super().__init__(constraint, position, gear_ratio)
    
 
 
-class WristJoint(Joint):
+class Wrist(Joint):
 # Wrist joint of Moveo arm.
-    def __init__(self, constraint = 30, position = 0, gear_ratio = 1.0):
+    def __init__(self, constraint = 200, position = 0, gear_ratio = 1.0):
         super().__init__(constraint, position, gear_ratio)
         
 
 
-class GripJoint(Joint):
+class Grip(Joint):
 # Grip joint of Moveo arm.
-    def __init__(self, constraint = 30, position = 0, gear_ratio = 1.0):
+    def __init__(self, constraint = 180, position = 0, gear_ratio = 1.0):
         super().__init__(constraint, position, gear_ratio)
     
--- README.md
+++ README.md
@@ -1,21 +1,18 @@
 # ChessMate
 
+ChessMate is a student Capstone project from the University of Alberta. Our objective was to design and create an open source based program that would enable a robotic arm to compete in a game of chess.
+
 ### ChessMate
-The main program for use with the BCN Moveo and our ChessMate game board.
+Python program that allows a game of Chess to be played between the 3D BCN Moveo Arm and a human user. 
 
 ### Merlin
-Custom robotic control program.
+Firmware for the 3D BCN Moveo Arm. Utilizes an Arduino Mega 2560. 
 
 ### CMController
-Connector for ChessMate and Merlin.
+CMController was created for use with our own custom Merlin firmware for control of the 3D BCN Moveo Arm. 
+It enables serial communication with Merlin to send instructions based on cartesian coordinates. The CMController is also used by the ChessMate program for it's integration with Merlin. Contains a combination of commands to enable ChessMate to play a game of chess with the 3D Moveo Arm.
 
 ### gameBoard
-Arduino code for running the RFID chess board.
+Arduino code for running the RFID chess board. Polls and collects RFID information from the game board to determine the current status of the game. 
 
 
-### Getting Started:
-- Clone our repo.
-- Activate the python virtual environment.
-
-
-Python environment packages are listed in the requirements.txt file. Can be used to generate a python virtual environment for use with CMController and eventually ChessMate.
